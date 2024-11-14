b1eb5b52ae5399c7768e381f09483f93b4a8fb34
Dennis Kemboi <denkogei11@gmail.com>
2024-11-14 21:11:25 +0300

    created errorpage

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ErrorPage = () => {
+  return (
+    <div className="error-page">
+      <img 
+        src="/assets/error.png"  
+        alt="Error Page Background" 
+        className="error-background-image" 
+      />
+      
+    </div>
+  );
+};
+
+export default ErrorPage;
